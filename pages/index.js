e29950619d9d8d4b48911b503172ec61fb14fd1c
mteusbarbosa <mateus.bm13@gmail.com>
2023-05-07 00:25:11 -0300

    implement getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -11,8 +21,21 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Me chamo Mateus Barbosa, sou de Natal-RN e tenho o anseio de me tornar Sênior em Front-end. Sou técnico em informática e tecnólogo em Sistemas para Internet, ambos pelo IFRN . Atualmente possuo habilidades em HTML, CSS, JavaScript, TypeScript e Framework Angular.</p>
-
         <Link href="/posts/first-post">Posts</Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
